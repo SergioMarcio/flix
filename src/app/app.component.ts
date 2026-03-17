2c8d813653343e4137192280c58b07973af659bb
sergiomarciosilva <sergiomarcio@gmail.com>
2026-03-17 09:48:43 -0300

    ajuste redirecionamento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { AsyncPipe } from '@angular/common';
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SplashScreenComponent } from './components/splash-screen/splash-screen.component';
 import { SupabaseService } from './services/supabase.service';
@@ -12,8 +12,12 @@ import { SupabaseService } from './services/supabase.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   splashDone = false;
 
-  constructor(public supabase: SupabaseService) { }
+  constructor(public supabase: SupabaseService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.navigate(['/']);
+  }
 }
