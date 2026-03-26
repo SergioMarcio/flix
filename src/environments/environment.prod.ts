e87dfc3b730e77becbce420353a463bb1ae72877
sergiomarciosilva <sergiomarcio@gmail.com>
2026-03-26 10:29:41 -0300

    ajuste menu email

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   tmdbApiKey: 'dfbbc3c2c51633aa47a8b40fdce42aec',
   tmdbBaseUrl: 'https://api.themoviedb.org/3',
   tmdbImageBaseUrl: 'https://image.tmdb.org/t/p',
-  supabaseUrl: 'YOUR_SUPABASE_URL',
-  supabaseAnonKey: 'YOUR_SUPABASE_ANON_KEY',
-  giphyApiKey: 'SUA_CHAVE_GIPHY_AQUI'
+  supabaseUrl: 'https://xxzdgdriysalpqqnxsrf.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4emRnZHJpeXNhbHBxcW54c3JmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MzM5NzMsImV4cCI6MjA4OTAwOTk3M30.9EGnb5TIF-DcQ3Uc1T1jCuDd3cPsTWxoHXfxtB1ubHM',
+  giphyApiKey: '2XSCIEIpZ1xPyyd9JgV5cwBLFrFMUIsm'
 };
